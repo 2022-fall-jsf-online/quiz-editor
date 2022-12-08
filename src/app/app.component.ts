0bd6f966722a8121e77cbdab65225961fa86da84
bottleinamessage <rollmop.flosses.0z@icloud.com>
2022-12-07 20:45:31 -0800

    Interpolated binding of deleted quiz badges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,10 +50,6 @@ export class AppComponent implements OnInit {
     }
   };
 
-  cancelChanges = () => { 
-    this.loadQuizzesFromCloud
-    this.selectedQuiz = undefined;
-  }
 
   ngOnInit() {
     this.loadQuizzesFromCloud();
@@ -157,4 +153,16 @@ export class AppComponent implements OnInit {
     }
   };
 
+  cancelChanges = () => { 
+    this.loadQuizzesFromCloud
+    this.selectedQuiz = undefined;
+  }
+
+  getDeletedQuizzes = () => {
+    return this.quizzes.filter(x => x.markedForDelete)
+  }
+
+  get deletedQuizCount() {
+    return this.getDeletedQuizzes().length;
+  }
 }
