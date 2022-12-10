53a63023ef3b556f4a44c7fcf4a00f4a8d8ec7f3
Rebecca Lee <rklee@madisoncollege.edu>
2022-12-10 17:17:36 -0600

    Added addNewQuestion & removeQuestion - Question Editing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 import { QuizService } from './quiz.service';
 
 interface QuizDisplay {
@@ -60,4 +61,25 @@ export class AppComponent implements OnInit {
 
     this.selectQuiz(newQuiz);
   };
+
+  // add new question to this quizzes' current questions
+  addNewQuestion = () => {
+    //can't add question to undefined so need if statement
+    if (this.selectedQuiz) {
+      // grab current questions (this.selectedQuiz?.quizQuestions) (selectedQuiz is a QuizDisplay > uses quizQuestions that is a questionDisplay)
+      // spread in current questions (questionDisplay) and add new question (questionDisplay)
+      this.selectedQuiz.quizQuestions = [
+        ...this.selectedQuiz.quizQuestions
+        , {
+          questionName: "Untitled Question"
+        }
+      ];
+    }
+  }
+
+  removeQuestion = (question: QuestionDisplay) => {
+    if (this.selectedQuiz) {
+      this.selectedQuiz.quizQuestions = this.selectedQuiz.quizQuestions.filter(x => x !== question);
+    }
+  }
 }
